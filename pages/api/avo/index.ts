5c9835d9f568159d2a41fce57c52499f2e7e341d
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-10-31 20:44:21 -0500

    first commit

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,27 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import DB from '@database'
+import enablePublicAccess from '@cors'
+
+const allAvos = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    await enablePublicAccess(req, res)
+
+    const db = new DB()
+    const allEntries = await db.getAll()
+    const length = allEntries.length
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.end(JSON.stringify({ length, data: allEntries }))
+  } catch (e) {
+    console.error(e)
+    res.statusCode = 500
+    res.end(
+      JSON.stringify({ length: 0, data: [], error: 'Something went wrong' })
+    )
+  }
+}
+
+export default allAvos
